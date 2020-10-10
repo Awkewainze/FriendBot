3003eecd77fabb041804f35905536ed7087b03c0
Awkewainze <nova@walseth.me>
2020-10-10 01:05:19 -0700

    Config changes for deployment

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,24 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-export const CONFIG: Config = require("../config.json");
+import * as fs from "fs";
+import * as path from "path";
+
+let config: Config;
+try {
+    if (fs.existsSync(path.join(__dirname, "config.json"))) {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        config = require("../config.json");
+    } else {
+        config = {
+            DISCORD: {
+                LOGIN_TOKEN: process.env.NODE_ENV["DISCORD_LOGIN_TOKEN"]
+            }
+        };
+    }
+} catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+}
+
+export const CONFIG: Config = config;
 
 export declare type Config = {
     DISCORD: {
